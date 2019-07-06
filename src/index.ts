993c0f0c26631329b9a7035c5e3705caa8095076
aoisupersix <aoisupersix@gmail.com>
2019-07-06 16:32:50 +0900

    [update] 実行できるようにした

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+import * as commander from 'commander'
+
+const command = new commander.Command()
+command
+    .version('0.0.1', '-v, --version')
+    .parse(process.argv)
+
 console.log('Hello World')
