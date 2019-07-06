8567d9fc4460f535e7a540525db5b1cfd7cefb29
aoisupersix <aoisupersix@gmail.com>
2019-07-06 19:05:08 +0900

    [add] サブコマンド'status'を追加

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as commander from 'commander'
 const command = new commander.Command()
 command
     .version('0.0.1', '-v, --version')
+    .command('status', 'Display the current connection destination of each config.')
     .parse(process.argv)
 
 console.log('Hello World')
