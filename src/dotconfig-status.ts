2ea1285f4617cf8234bf450f5ca117e4f339e8b6
a_tanaka <a_tanaka@timedia.co.jp>
2019-07-17 13:37:25 +0900

    [add] dotconfig initとstatusを追加

---
+++ src/dotconfig-status.ts
@@ -0,0 +1,6 @@
+import * as commander from 'commander'
+
+const program = new commander.Command()
+program.parse(process.argv)
+
+console.log('status')
--- src/dotconfig.ts
+++ src/dotconfig.ts
@@ -3,9 +3,7 @@ import * as commander from 'commander'
 const program = new commander.Command()
 program
     .version('0.0.1', '-v, --version')
+    .command('init', 'Generate a preset in the current directory.')
     .command('status', 'Display the current connection destination of each config.')
-    .action(() => {
-        console.log('status')
-    })
 
 program.parse(process.argv)
